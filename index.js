6eb9acc389acf1740b13a20feafbacd8ccf34ab6
danielmekuriaw <daniel.mekuriaw16@gmail.com>
2021-07-21 15:33:47 -0500

    document onload DONE

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    const txt = document.getElementById("text")
+    txt.textContent = "This is really cool!"
+  })
